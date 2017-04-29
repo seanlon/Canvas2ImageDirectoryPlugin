dc02f37fa579a3669e2a9cdbac1b5ed0847d3ac2
SeanLon <lseanlon@gmail.com>
2017-04-29 12:40:40 +0800

    intiial

---
+++ src/ios/Canvas2ImageDirectoryPlugin.h
@@ -0,0 +1,22 @@
+//
+//  Canvas2ImageDirectoryPlugin.h
+//  Canvas2ImageDirectoryPlugin PhoneGap/Cordova plugin
+//
+//  Created by Tommy-Carlos Williams on 29/03/12.
+//  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
+//	MIT Licensed
+//
+
+
+#import <Cordova/CDVPlugin.h>
+
+@interface Canvas2ImageDirectoryPlugin : CDVPlugin
+{
+	NSString* callbackId;
+}
+
+@property (nonatomic, copy) NSString* callbackId;
+
+- (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ www/Canvas2ImageDirectoryPlugin.js
@@ -0,0 +1,27 @@
+//
+//  Canvas2ImageDirectoryPlugin.js
+//  Canvas2ImageDirectoryPlugin PhoneGap/Cordova plugin
+//
+//  Created by Tommy-Carlos Williams on 29/03/12.
+//  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
+//  MIT Licensed
+//
+
+  module.exports = {
+
+    saveImageDataToLibrary:function(successCallback, failureCallback, canvasId, directory, filename) {
+        // successCallback required
+        if (typeof successCallback != "function") {
+            console.log("Canvas2ImageDirectoryPlugin Error: successCallback is not a function");
+        }
+        else if (typeof failureCallback != "function") {
+            console.log("Canvas2ImageDirectoryPlugin Error: failureCallback is not a function");
+        }
+        else {
+            var canvas = (typeof canvasId === "string") ? document.getElementById(canvasId) : canvasId;
+            var imageData = canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+            return cordova.exec(successCallback, failureCallback, "Canvas2ImageDirectoryPlugin","saveImageDataToLibrary",[imageData, directory, filename]);
+        }
+    }
+  };
+
